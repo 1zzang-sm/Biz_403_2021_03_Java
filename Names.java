466bd461185310e528915ff04ba58c29695073b4
1zzang-sm <captain1152@naver.com>
2021-03-16 14:00:19 +0900

    처음올리는 git reposi

---
+++ Names.java
@@ -0,0 +1,25 @@
+/* 
+여기서 부터 자바 코드가 시작된다.
+comment, 주석 문법과 관계 없이 
+자유롭게 아무 내용이나 입력할 수 있는 영역
+Names 클래스 라고 읽는다.
+Names 클래스는 Names.java 라는 이름으로 파일을 저장해야 한다. 
+*/
+public class Names {
+	// main() {}, main() 
+	// main 메소드 라고 읽는다. 
+	public static void main(String[] arg) {
+		/*
+		"" : 문자열
+		""(DQ) 로 묶여 있는 내용을 문자열
+
+		"" 로 묶여있는 문자열을 출력해라 
+		*/
+		System.out.println("I am Lsm");
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		System.out.println("30 + 40");
+		System.out.println(30 * 40 + 100);
+
+	}
+}
